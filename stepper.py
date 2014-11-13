0a448c358e50fad4275343737911ec26ffa1d9cf
Juan Santiago Medina <jsm601@3d-imacs-station.cusp.nyu.edu>
2014-11-13 13:44:16 -0500

    stepper

--- stepper.py
+++ stepper.py
@@ -46,11 +46,11 @@ def setStep(w1, w2, w3, w4):
 	GPIO.output(coil_B_1_pin, w3)
 	GPIO.output(coil_B_2_pin, w4)
 
-# while True:
-# 	delay = raw_input("Delay between steps (milliseconds)?")
-# 	steps = raw_input("How many steps forward? ")
-# 	forward(int(delay) / 1000.0, int(steps))
-# 	steps = raw_input("How many steps backwards? ")
-# 	backwards(int(delay) / 1000.0, int(steps))
+while True:
+	delay = raw_input("Delay between steps (milliseconds)?")
+	steps = raw_input("How many steps forward? ")
+	forward(int(delay) / 1000.0, int(steps))
+	steps = raw_input("How many steps backwards? ")
+	backwards(int(delay) / 1000.0, int(steps))
 
-# GPIO.cleanup()
+GPIO.cleanup()
