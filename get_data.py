790f4529e95d127ac1039cdd0f1ad6c5f6dba7cb
Juan Santiago Medina <jsm601@3d-imacs-station.cusp.nyu.edu>
2014-11-13 14:30:29 -0500

    stepper

--- get_data.py
+++ get_data.py
@@ -76,21 +76,21 @@ def get_data(samples, item_class):
 	cam = cv2.VideoCapture(0)
 	with picamera.PiCamera() as camera:
 		
-		camera.resolution = (1280, 720)
-		camera.framerate = 30
-		# Give the camera's auto-exposure and auto-white-balance algorithms
-		# some time to measure the scene and determine appropriate values
-		camera.iso = 200
-		time.sleep(2)
-		# Now fix the values
-		camera.shutter_speed = camera.exposure_speed
-		camera.exposure_mode = 'off'
-		g = camera.awb_gains
-		camera.awb_mode = 'off'
-		camera.awb_gains = g
+		# camera.resolution = (1280, 720)
+		# camera.framerate = 30
+		# # Give the camera's auto-exposure and auto-white-balance algorithms
+		# # some time to measure the scene and determine appropriate values
+		# camera.iso = 200
+		# time.sleep(2)
+		# # Now fix the values
+		# camera.shutter_speed = camera.exposure_speed
+		# camera.exposure_mode = 'off'
+		# g = camera.awb_gains
+		# camera.awb_mode = 'off'
+		# camera.awb_gains = g
 		# Finally, take several photos with the fixed settings
-		if samples > 512 or samples < 0:
-			samples = 512
+		# if samples > 512 or samples < 0:
+		# 	samples = 512
 		steps = int(512 /samples)
 		for s in range(samples- 1):
 			camera.capture('pi_cam%s' %s + '.jpg')
