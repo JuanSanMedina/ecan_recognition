e995c4fc41d867ea3e5392a26b474fa6b8a9d2f1
Juan Santiago Medina <jsm601@3d-imacs-station.cusp.nyu.edu>
2014-11-17 16:32:38 -0500

    get data

--- upload_item.py
+++ upload_item.py
@@ -1,11 +1,11 @@
 import requests
 # from pprint import pprint
 
-#url = 'http://127.0.0.1:8000/ecan/upload/'
-url = 'http://ecan-recognition.herokuapp.com/ecan/upload/'
+url = 'http://127.0.0.1:8000/ecan/upload/'
+# url = 'http://ecan-recognition.herokuapp.com/ecan/upload/'
 
-data = {'ecan':'1', 'weight':'2.3'}
-files = {'image_color': open('can.jpeg', 'rb'),'image_ir':open('ir_can.jpg', 'rb')}
+data = {'ecan':'1', 'weight':'12', 'item_class':'1'}
+files = {'image_picam': open('pi.jpg', 'rb')}
 
 r = requests.post(url, data = data, files=files)
 print r.text
