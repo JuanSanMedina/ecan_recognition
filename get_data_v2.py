17eb84fdd66cf25235d06c5cd1b00d2cd87e2d6f
Juan Santiago Medina <jsm601@3d-imacs-station.cusp.nyu.edu>
2014-11-20 13:31:36 -0500

    get data

--- get_data_v2.py
+++ get_data_v2.py
@@ -1,3 +1,8 @@
+# import cv2
+# cam = cv2.VideoCapture(0)	
+# correct, img = cam.read()
+# if correct: cv2.imwrite('usb_cam/usb_cam.jpg',img) #save image
+import io
 import time
 import picamera
 # import pygame.camera
@@ -6,6 +11,8 @@ import get_weight
 import requests
 import RPi.GPIO as GPIO
 import time
+from PIL import Image
+import StringIO
 # import cv2
 
 GPIO.setmode(GPIO.BCM)
@@ -56,10 +63,25 @@ def setStep(w1, w2, w3, w4):
 	GPIO.output(coil_B_1_pin, w3)
 	GPIO.output(coil_B_2_pin, w4)
 
-def take_picamera():
-	with picamera.PiCamera() as camera:
-		camera.led = False
-		camera.capture('ir.jpg')
+
+def outputs(samples, steps, weight, bg_pk):
+	stream = io.BytesIO()
+	for i in range(samples):
+		yield stream
+		stream.seek(0)
+		img = Image.open(stream)
+		my_file = StringIO.StringIO()
+		img.save(my_file, "JPEG")
+		my_file.seek(0)
+		data = {'ecan':'1','bg': bg_pk, 'weight':weight, 'item_class':item_class}
+		files = {'image_picam': my_file}
+		url = 'http://128.122.72.105:8000/ecan/upload/'
+		r = requests.post(url, data = data, files=files)
+		print r.text
+		stream.seek(0)
+		stream.truncate()
+		my_file.close()
+		forward(5, steps)
 
 def get_data(samples, item_class):
 	cont = 'n'
@@ -69,18 +91,16 @@ def get_data(samples, item_class):
 		if cont != 'y':
 			cont = 'n'
 
-
 	with picamera.PiCamera() as camera:
-		camera.resolution = (640, 480)
+		camera.resolution = (1024, 768)
 		camera.iso = 200
-		camera.framerate = 30
+		camera.framerate = 40
 		time.sleep(2)
 		# camera.shutter_speed = camera.exposure_speed
 		# camera.exposure_mode = 'off'
 		g = camera.awb_gains
 		camera.awb_mode = 'off'
 		camera.awb_gains = g
-
 
 		camera.capture('pi_cam/bg_im.jpg')
 		data = {'ecan':'1'}
@@ -92,39 +112,16 @@ def get_data(samples, item_class):
 			print r.json()['result'], 'Back ground id: ', r.json()['id']
 		else: return 'Operation not completed'
 
-
 		print 'Place item'
 		cont = 'n'
 		while cont != 'y':
 			cont = raw_input("ready? [y] ")
 			if cont != 'y':
 				cont = 'n'
-
+		weight = get_weight.get()
 
 		steps = int(512 /samples)
-		# cam = cv2.VideoCapture(0)	
-		for s in range(samples):
-			print s
-			if s == 0: weight = get_weight.get()
-			camera.capture('pi_cam/pi_im.jpg')
-			data = {'ecan':'1', 'bg': bg_pk, 'weight':weight, 'item_class':item_class}
-			files = {'image_picam': open('pi_cam/pi_im.jpg', 'rb')}
-			url = 'http://128.122.72.105:8000/ecan/upload/'
-			# cam = cv2.VideoCapture(0)	
-			# cam.set(3,1280)
-			# cam.set(4,1024)
-			# cam.set(13,70) #saturation
-			# time.sleep(10/1000.0)
-			# cam.set
-			# correct, img = cam.read()
-			# if correct: cv2.imwrite('usb_cam/usb_cam%s' %s + '.jpg',img) #save image
-			# url = 'http://ecan-recognition.herokuapp.com/ecan/upload/'
-			# print data
-			r = requests.post(url, data = data, files=files)
-			print r.text
-			
-			forward(5, steps)
-		# cam.release()
+		camera.capture_sequence(outputs(samples, steps, weight, bg_pk, item_class), 'jpeg', use_video_port=True)
 	return 'done'
 
 cont = 'y'
@@ -139,39 +136,24 @@ while cont == 'y':
 GPIO.cleanup()
 
 
-# camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
-# data = {'ecan':'1', 'weight':get_weight.get()}
-# files = {'image_color': open('color.jpg', 'rb'),'image_ir':open('ir.jpg', 'rb')}
-# url = 'http://127.0.0.1:8000/ecan/upload/'
-# # url = 'http://ecan-recognition.herokuapp.com/ecan/upload/'
-# r = requests.post(url, data = data, files=files)
-# print r.text
+
+		# if upload_type == 'bg':
+		# 	data = {'ecan':'1'}
+		# 	files = {'back_ground': my_file}
+		# 	url = 'http://128.122.72.105:8000/ecan/upload-back_ground/'
+		# 	r = requests.post(url, data = data, files=files)
+		# 	if r.json()['result'] == 'valid':
+		# 		bg_pk =r.json()['id']
+		# 		return bg_pk
+		# 		print r.json()['result'], 'Back ground id: ', r.json()['id']
+		# 	else: return 'Operation not completed'
+		# if upload_type == 'item':
 
 
-# def take_usb():
-# 	pygame.camera.init()
-# 	cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
-# 	cam.start()
-# 	img = cam.get_image()
-# 	pygame.image.save(img, "color.jpg")
-# 	pygame.camera.quit()
-
-# take_picamera()
-# take_usb()
-
-
-# camera.resolution = (1280, 720)
-# camera.framerate = 30
-# # Give the camera's auto-exposure and auto-white-balance algorithms
-# # some time to measure the scene and determine appropriate values
-# camera.iso = 200
-# time.sleep(2)
-# # Now fix the values
-# camera.shutter_speed = camera.exposure_speed
-# camera.exposure_mode = 'off'
-# g = camera.awb_gains
-# camera.awb_mode = 'off'
-# camera.awb_gains = g
-# Finally, take several photos with the fixed settings
-# if samples > 512 or samples < 0:
-# 	samples = 512
+		# if samples <= 40: camera.capture_sequence(outputs(samples, steps, weight), 'jpeg', use_video_port=True)
+		# else: 
+		# 	ints = int(samples/40)
+		# 	new_samples = [40 for i in range(ints)]
+		# 	if samples % 40 > 0: new_samples.append(samples % 40)
+		# 	for e in new_samples:
+		# 		camera.capture_sequence(outputs(e, steps, weight), 'jpeg', use_video_port=True)
