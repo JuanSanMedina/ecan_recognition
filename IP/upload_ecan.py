e2faeb813f77a0b51c50fea2236ade2b119b3647
Juan Medina <jsmedmar@users.noreply.github.com>
2017-05-22 15:54:27 -0400

    Update upload_ecan.py

--- IP/upload_ecan.py
+++ IP/upload_ecan.py
@@ -1,21 +1,11 @@
 import requests
 import sys
-
-# import socket
-# from pprint import pprint
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.connect(("goolge.com",0))
-# ip = str(s.getsockname()[0])
-# s.close()
-# print ip
-# ip = str(socket.gethostbyname(socket.gethostname()))
-# from pprint import pprint
-# url = 'http://127.0.0.1:8000/ecan/upload-ecan/'
 	
-def send(ip):	
-	url = 'http://ecan-recognition.herokuapp.com/ecan/upload-ecan/'
-	data = {'pk':'1', 'address':'CUSP at PPP', 'latitude':1.0, 'longitude':1.0, 'ip': ip}
-	r = requests.post(url, data = data)
-	print r.text
+def send(ip):
+   """Update IP of Raspberry PI in server."""
+   url = 'http://ecan-recognition.herokuapp.com/ecan/upload-ecan/'
+   data = {'pk':'1', 'address':'CUSP at PPP', 'latitude':1.0, 'longitude':1.0, 'ip': ip}
+   r = requests.post(url, data = data)
+   print(r.text)
 
 send(sys.argv[1])
